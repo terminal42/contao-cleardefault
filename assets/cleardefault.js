f818d152155978731e33433f4a77f3d7d91f2b1a
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-03-21 11:31:25 +0100

    Use .min style file names for javascript

--- assets/cleardefault.js
+++ assets/cleardefault.js
@@ -1,1 +1,70 @@
-window.addEvent("domready",function(){if(!("placeholder" in new Element("input",{type:"text"}))){Element.implement("cleardefault",function(){var a=this;if(a.get("tag")=="input"&&a.get("type")=="password"){var b=new Element("input",{type:"text","class":(a.get("class")+" cleardefault"),value:a.get("placeholder"),events:{focus:function(){b.hide();a.show().focus()}}}).inject(a,"before");a.hide().addEvent("blur",function(){if(a.value==""){a.hide();b.show()}})}else{if(a.get("placeholder")&&((a.get("tag")=="input"&&["text","search","url","tel","email"].contains(a.get("type")))||(a.get("tag")=="textarea"))){if(a.value==""){a.addClass("cleardefault");a.value=a.get("placeholder")}a.addEvents({focus:function(){if(a.value==a.get("placeholder")){a.removeClass("cleardefault");a.value=""}},blur:function(){if(a.value==""){a.addClass("cleardefault");a.value=a.get("placeholder")}}})}}});document.getElements("input[placeholder], textarea").cleardefault()}else{Element.implement("cleardefault",function(){})}});
+window.addEvent('domready', function()
+{
+	// Check if Browser supports the "placeholder" attribute
+	if (!('placeholder' in new Element('input', {type:'text'})))
+	{
+		Element.implement('cleardefault', function()
+		{
+			var el = this;
+			if (el.get('tag') == 'input' && el.get('type') == 'password')
+			{
+				var text = new Element('input', {
+					'type': 'text',
+					'class': (el.get('class')+' cleardefault'),
+					'value': el.get('placeholder'),
+					'events': {
+						'focus': function() {
+							text.hide();
+							el.show().focus();
+						}
+					}
+				}).inject(el, 'before');
+				
+				el.hide().addEvent('blur', function()
+				{
+					if (el.value == '')
+					{
+						el.hide();
+						text.show();
+					}
+				});
+			}
+			else if (el.get('placeholder') && ((el.get('tag') == 'input' && ['text', 'search', 'url', 'tel', 'email'].contains(el.get('type'))) || (el.get('tag') == 'textarea')))
+			{
+				if (el.value == '')
+				{
+					el.addClass('cleardefault');
+					el.value = el.get('placeholder');
+				}
+				
+				el.addEvents(
+				{
+					'focus': function()
+					{
+						if (el.value == el.get('placeholder'))
+						{
+							el.removeClass('cleardefault');
+							el.value = '';
+						}
+					},
+					'blur': function()
+					{
+						if (el.value == '')
+						{
+							el.addClass('cleardefault');
+							el.value = el.get('placeholder');
+						}
+					}
+				});
+			}
+		});
+		
+		document.getElements('input[placeholder], textarea').cleardefault();
+	}
+	
+	// Provide an empty "cleardefault" function for modern browsers, so a call to el.cleardefault() will not throw an error
+	else
+	{
+		Element.implement('cleardefault', function(){});
+	}
+});
